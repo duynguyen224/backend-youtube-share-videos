744ac7b8a058473d75b08ef85d60aa9afacd6873
duynguyen224 <42030245+duynguyen224@users.noreply.github.com>
2022-08-15 17:42:22 +0700

    oke

--- models/User.js
+++ models/User.js
@@ -1,29 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        minLength:6,
-        maxLength: 40,
-        unique: true
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            minLength: 6,
+            maxLength: 40,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            minLength: 6,
+            maxLength: 50,
+            unique: true,
+        },
+        imageUrl: {
+            type: String,
+            required: true,
+            minLength: 6,
+        },
+        googleId: {
+            type: String,
+            required: true,
+            minLength: 6,
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        minLength:10,
-        maxLength: 50,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength:6
-    },
-    admin: {
-        type: Boolean,
-        default: false,
-    }
-},  {timestamps: true})
+    { timestamps: true }
+);
 
-module.exports =mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
